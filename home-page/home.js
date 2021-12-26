146161512284b73326e4cc48273dcfaa01ba580d
jhamadhav <jhamadhav28@gmail.com>
2021-12-26 21:03:34 +0530

    Co-authored-by: Pratik21Pimpalkar <Pratik21Pimpalkar@users.noreply.github.com> Co-authored-by: Vedansh Dubey <vedanshstudy@gmail.com> Co-authored-by: jiya1402 <jiya1402@users.noreply.github.com>

---
+++ home-page/home.js
@@ -0,0 +1,15 @@
+window.onload = () => {
+    let cards = document.getElementsByClassName("card")
+
+    let gifs = document.getElementsByTagName("lottie-player")
+
+    for (let i = 0; i < cards.length; i++) {
+        cards[i].addEventListener("mouseenter", () => {
+            gifs[i].style.visibility = "visible"
+        })
+
+        cards[i].addEventListener("mouseleave", () => {
+            gifs[i].style.visibility = "hidden"
+        })
+    }
+}
