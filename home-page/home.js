2200e24477f533da9a6b82c0cb077aa630b9ea58
jhamadhav <jhamadhav28@gmail.com>
2021-12-27 13:32:10 +0530

    minor changes

--- home-page/home.js
+++ home-page/home.js
@@ -3,13 +3,17 @@ window.onload = () => {
 
     let gifs = document.getElementsByClassName("lottie-player")
 
+    let homeImg = document.getElementsByClassName("home-img")[0]
+
     for (let i = 0; i < cards.length; i++) {
         cards[i].addEventListener("mouseenter", () => {
             gifs[i].style.visibility = "visible"
+            homeImg.style.visibility = "hidden"
         })
 
         cards[i].addEventListener("mouseleave", () => {
             gifs[i].style.visibility = "hidden"
+            homeImg.style.visibility = "visible"
         })
     }
 }
