9e403a3720f05e38d1ced91c8015584142ac5284
Madhav Jha <jhamadhav28@gmail.com>
2021-12-27 12:47:54 +0530

    About us page added (#9)
    * Add files via upload
    
    * Add files via upload
    
    * Rename index.html to about-index.html
    
    * about us folder structure improved
    
    Co-authored-by: Harsheet Chordiya <76674531+Harsheet196@users.noreply.github.com>

---
+++ about-us/assets/js/first.js
@@ -0,0 +1,51 @@
+//background animation
+let tl = anime.timeline({
+    easing: 'easeOutExpo',
+    duration: 850
+  });
+  
+  tl.add({
+    targets: 'section .item',
+    width: '100%',
+    backgroundColor: '#F6A9BD',
+    delay: anime.stagger(100)
+  });
+  
+  tl.add({
+    targets: 'section .item',
+    delay: anime.stagger(70),
+    width: '97%',
+    backgroundColor: '#F4E0E1'
+  });
+  
+  tl.add({
+    targets: 'section .item',
+    backgroundColor: '#FFFFFF',
+    delay: anime.stagger(50,{from: 'center'})
+  });
+  
+  tl.add({
+    targets: '.text',
+    top: '49%',
+    duration: 4500,
+    opacity: 1
+  }, '-=1000')
+  
+  //text animation
+  
+  //wrap every letter in a span
+  var textWrapper = document.querySelector('.effect1');
+  textWrapper.innerHTML = textWrapper.textContent.replace(/([^.\s]|\w)/g, "<span class='letter'>$&</span>");
+  
+  anime.timeline()
+    .add({
+      targets: '.effect1 .letter',
+      scale: [5,1],
+      opacity: [0,1],
+      translateZ: 0,
+      easing: "easeOutExpo",
+      duration: 1350,
+      delay: function(el, i) {
+        return 70*i;
+    }
+  }, 1500);
