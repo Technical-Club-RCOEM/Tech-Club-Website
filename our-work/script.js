701c1f07b4777201855b7bff854691972455c488
jhamadhav <jhamadhav28@gmail.com>
2022-01-09 14:02:47 +0530

    Our work section fixed
    Co-authored-by: Pratik21Pimpalkar <Pratik21Pimpalkar@users.noreply.github.com>
    Co-authored-by: Anjali Kushwaha <Anjali2201@users.noreply.github.com>
    Co-authored-by: Vedansh Dubey <vedanshstudy@gmail.com>

--- our-work/script.js
+++ our-work/script.js
@@ -12,8 +12,9 @@ const appBgContainerEl = document.querySelector(".app__bg");
 const cardInfosContainerEl = document.querySelector(".info__wrapper");
 
 buttons.next.addEventListener("click", () => swapCards("right"));
-
 buttons.prev.addEventListener("click", () => swapCards("left"));
+
+document.getElementsByClassName("nxt")[0].addEventListener("click", () => swapCards("right"))
 
 function swapCards(direction) {
 	const currentCardEl = cardsContainerEl.querySelector(".current--card");
@@ -77,44 +78,44 @@ function changeInfo(direction) {
 
 	gsap.timeline()
 		.to([buttons.prev, buttons.next], {
-		duration: 0.2,
-		opacity: 0.5,
-		pointerEvents: "none",
-	})
+			duration: 0.2,
+			opacity: 0.5,
+			pointerEvents: "none",
+		})
 		.to(
-		currentInfoEl.querySelectorAll(".text"),
-		{
-			duration: 0.4,
-			stagger: 0.1,
-			translateY: "-120px",
-			opacity: 0,
-		},
-		"-="
-	)
+			currentInfoEl.querySelectorAll(".text"),
+			{
+				duration: 0.4,
+				stagger: 0.1,
+				translateY: "-120px",
+				opacity: 0,
+			},
+			"-="
+		)
 		.call(() => {
-		swapInfosClass(direction);
-	})
+			swapInfosClass(direction);
+		})
 		.call(() => initCardEvents())
 		.fromTo(
-		direction === "right"
-		? nextInfoEl.querySelectorAll(".text")
-		: previousInfoEl.querySelectorAll(".text"),
-		{
-			opacity: 0,
-			translateY: "40px",
-		},
-		{
-			duration: 0.4,
-			stagger: 0.1,
-			translateY: "0px",
-			opacity: 1,
-		}
-	)
+			direction === "right"
+				? nextInfoEl.querySelectorAll(".text")
+				: previousInfoEl.querySelectorAll(".text"),
+			{
+				opacity: 0,
+				translateY: "40px",
+			},
+			{
+				duration: 0.4,
+				stagger: 0.1,
+				translateY: "0px",
+				opacity: 1,
+			}
+		)
 		.to([buttons.prev, buttons.next], {
-		duration: 0.2,
-		opacity: 1,
-		pointerEvents: "all",
-	});
+			duration: 0.2,
+			opacity: 1,
+			pointerEvents: "all",
+		});
 
 	function swapInfosClass() {
 		currentInfoEl.classList.remove("current--info");
@@ -190,21 +191,21 @@ function init() {
 		"--card-translateY-offset": "0%",
 	})
 		.to(cardInfosContainerEl.querySelector(".current--info").querySelectorAll(".text"), {
-		delay: 0.5,
-		duration: 0.4,
-		stagger: 0.1,
-		opacity: 1,
-		translateY: 0,
-	})
-		.to(
-		[buttons.prev, buttons.next],
-		{
+			delay: 0.5,
 			duration: 0.4,
+			stagger: 0.1,
 			opacity: 1,
-			pointerEvents: "all",
-		},
-		"-=0.4"
-	);
+			translateY: 0,
+		})
+		.to(
+			[buttons.prev, buttons.next],
+			{
+				duration: 0.4,
+				opacity: 1,
+				pointerEvents: "all",
+			},
+			"-=0.4"
+		);
 }
 
 const waitForImages = () => {
@@ -240,10 +241,10 @@ const waitForImages = () => {
 				if (totalImages == loadedImages) {
 					gsap.timeline()
 						.to(".loading__wrapper", {
-						duration: 0.8,
-						opacity: 0,
-						pointerEvents: "none",
-					})
+							duration: 0.8,
+							opacity: 0,
+							pointerEvents: "none",
+						})
 						.call(() => init());
 				}
 			}
